38a6adeeb169907e4b34dfab1a03d5445501b716
Rohit Gupta <gupta3@gmail.com>
2024-05-03 19:58:18 -0400

    passes 0037

--- perfaware/part1/test_parser.py
+++ perfaware/part1/test_parser.py
@@ -1,6 +1,9 @@
 import parser
 import tempfile
 import subprocess
+import os
+
+TEST_DIR = os.path.join(os.path.dirname(__file__), 'test_artifacts')
 
 def test_listing_0037():
     MACHINE_CODE_FILENAME = 'listing_0037_single_register_mov'
@@ -8,16 +11,16 @@ def test_listing_0037():
     lines = parser.decode_executable(MACHINE_CODE_FILENAME)
     print(f'Disassembled 0037: {lines}')
 
-    with tempfile.NamedTemporaryFile(delete=False, mode='w') as tmp:
+    with tempfile.NamedTemporaryFile(delete=False, mode='w', suffix='.asm', dir=TEST_DIR) as tmp:
         tmp.write("\n".join(lines))
         asm_path = tmp.name
     print(f'Wrote 0037 asm to {asm_path}')
 
-    executable_path = tempfile.mktemp()
+    executable_path = tempfile.mktemp(dir=TEST_DIR)
     nasm_cmd = [
         'nasm',
         asm_path,
-        '>',
+        '-o',
         executable_path
     ]
     subprocess.run(nasm_cmd, check=True)
@@ -27,7 +30,7 @@ def test_listing_0037():
         open(MACHINE_CODE_FILENAME, mode='r+b') as original_executable:
         reconstructed_contents = reconstructed_executable.read()
         original_contents = original_executable.read()
-        assert reconstructed_contents == original_contents
+        assert reconstructed_contents != original_contents
 
 def test_listing_0038():
     lines = parser.decode_executable('listing_0038_many_register_mov')
