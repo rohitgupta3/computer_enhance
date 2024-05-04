de9d9e58e31937c43081cf17519419a556a5c75a
Rohit Gupta <gupta3@gmail.com>
2024-05-03 20:05:38 -0400

    clean

--- perfaware/part1/test_parser.py
+++ perfaware/part1/test_parser.py
@@ -5,17 +5,14 @@ import os
 
 TEST_DIR = os.path.join(os.path.dirname(__file__), 'test_artifacts')
 
-def test():
-    # MACHINE_CODE_FILENAME = 'listing_0037_single_register_mov'
-    MACHINE_CODE_FILENAME = 'listing_0038_many_register_mov'
-
-    lines = parser.decode_executable(MACHINE_CODE_FILENAME)
-    print(f'Disassembled 0037: {lines}')
+def test(filename):
+    lines = parser.decode_executable(filename)
+    print(f'Disassembled {filename}: {lines}')
 
     with tempfile.NamedTemporaryFile(delete=False, mode='w', suffix='.asm', dir=TEST_DIR) as tmp:
         tmp.write("\n".join(lines))
         asm_path = tmp.name
-    print(f'Wrote 0037 asm to {asm_path}')
+    print(f'Wrote {filename} asm to {asm_path}')
 
     executable_path = tempfile.mktemp(dir=TEST_DIR)
     nasm_cmd = [
@@ -25,12 +22,14 @@ def test():
         executable_path
     ]
     subprocess.run(nasm_cmd, check=True)
-    print(f'Assembled disassembled 0037 asm to {executable_path}')
+    print(f'Assembled disassembled {filename} asm to {executable_path}')
 
     with open(executable_path, mode='r+b') as reconstructed_executable, \
-        open(MACHINE_CODE_FILENAME, mode='r+b') as original_executable:
+        open(filename, mode='r+b') as original_executable:
         reconstructed_contents = reconstructed_executable.read()
         original_contents = original_executable.read()
         assert reconstructed_contents == original_contents
 
-test()
+
+test('listing_0037_single_register_mov')
+test('listing_0038_many_register_mov')
